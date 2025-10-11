3b21a9fa7865dbf8935a350de912005fde82f5ac
a.bizaval <bizaval.asghar@gmail.com>
2025-10-11 08:57:53 +0330

    2

---
+++ PcInfoWin/attributes/AllAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcInfoWin.Attributes
+{
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        public string Name { get; }
+        public TableAttribute(string name) => Name = name;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnAttribute : Attribute
+    {
+        public string Name { get; }
+        public ColumnAttribute(string name) => Name = name;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KeyAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DbGeneratedAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ForeignKeyAttribute : Attribute
+    {
+        public string RelatedTable { get; }
+        public string RelatedColumn { get; }
+        public ForeignKeyAttribute(string relatedTable, string relatedColumn)
+        {
+            RelatedTable = relatedTable;
+            RelatedColumn = relatedColumn;
+        }
+    }
+}
