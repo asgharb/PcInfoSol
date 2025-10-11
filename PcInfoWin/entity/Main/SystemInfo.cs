345f3e17a3606c7b06a5caa8c7a4c6d8814541f5
a.bizaval <bizaval.asghar@gmail.com>
2025-10-11 09:09:18 +0330

    88

--- PcInfoWin/entity/Main/SystemInfo.cs
+++ PcInfoWin/entity/Main/SystemInfo.cs
@@ -19,7 +19,6 @@ namespace PcInfoWin.Entity.Main
         [DbGenerated]
         [Column("SystemInfoID")]
         public int SystemInfoID { get; set; }
-
         [Ignore]
         public SystemEnvironmentInfo systemEnvironmentInfo { get; set; }
         [Ignore]
@@ -42,7 +41,9 @@ namespace PcInfoWin.Entity.Main
         public List<MonitorInfo> monitorInfos { get; set; }
 
         public bool IsActive { get; set; }
+
         public DateTime InsertDate { get; set; } = DateTime.Now;
+
         public DateTime? ExpireDate { get; set; } = null;
 
         public SystemInfo()
@@ -83,163 +84,6 @@ namespace PcInfoWin.Entity.Main
             // ---------- Monitor ----------
             var monitorProvider = new MonitorInfoProvider();
             monitorInfos = monitorProvider.GetAllMonitors();
-        }
-
-        public static SystemInfo GetSystemInfoWithChildrenFromDB(int sysId)
-        {
-            var db = new DataHelper();
-
-            var sysInfo = db.SelectById<SystemInfo>(sysId);
-
-            if (sysInfo == null) return null;
-
-            // RAM Summary
-            sysInfo.RamSummaryInfo = db.SelectByColumns<RamSummaryInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } }).FirstOrDefault();
-
-            // RAM Modules
-            sysInfo.RamModuleInfo = db.SelectByColumns<RamModuleInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } });
-
-            // Disks
-            sysInfo.DiskInfo = db.SelectByColumns<DiskInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } });
-
-            // Network Adapters
-            sysInfo.NetworkAdapterInfo = db.SelectByColumns<NetworkAdapterInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } });
-
-            // Optical Drives
-            sysInfo.OpticalDriveInfo = db.SelectByColumns<OpticalDriveInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } });
-
-            // Monitors
-            sysInfo.monitorInfos = db.SelectByColumns<MonitorInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } });
-
-            // CPU
-            sysInfo.cpuInfo = db.SelectByColumns<CpuInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } }).FirstOrDefault();
-            //Gpu
-            sysInfo.gppuInfo = db.SelectByColumns<GpuInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } }).FirstOrDefault();
-
-            //Motherboard
-            sysInfo.motherboardInfo = db.SelectByColumns<MotherboardInfo>(
-                new Dictionary<string, object> { { "SystemInfoRef", sysId } }).FirstOrDefault();
-
-            return sysInfo;
-        }
-
-        public static int InserNewInfoToDB()
-        {
-            try
-            {
-                var db = new DataHelper();
-
-                // 1. ساخت SystemInfo
-                var sysInfo = new SystemInfo();
-
-                // 2. پر کردن اطلاعات سخت‌افزار
-                sysInfo.cpuInfo = new CpuInfoProvider().GetCpuInfo();
-                sysInfo.gppuInfo = new GpuInfoProvider().GetGpuInfo();
-                sysInfo.motherboardInfo = new MotherboardInfoProvider().GetMotherboardInfo();
-                sysInfo.RamSummaryInfo = new RamInfoProvider().GetRamSummary();
-                sysInfo.RamModuleInfo = new RamInfoProvider().GetRamModules();
-                sysInfo.DiskInfo = new DiskInfoProvider().GetDisks();
-                sysInfo.OpticalDriveInfo = new OpticalDriveInfoProvider().GetAllOpticalDrives();
-                sysInfo.monitorInfos = new MonitorInfoProvider().GetAllMonitors();
-                sysInfo.NetworkAdapterInfo = new NetworkAdapterInfoProvider().GetAllNetworkAdapters();
-                sysInfo.systemEnvironmentInfo = new SystemEnvironmentProvider().GetSystemEnvironmentInfo();
-
-                // 3. درج SystemInfo و گرفتن ID
-                int sysId = (int)db.InsertAutoKey(sysInfo);
-                Console.WriteLine($"SystemInfoID: {sysId}");
-
-                // 4. درج CPU
-                if (sysInfo.cpuInfo != null)
-                {
-                    sysInfo.cpuInfo.SystemInfoRef = sysId;
-                    db.Insert(sysInfo.cpuInfo);
-                }
-
-                // 5. درج GPU
-                if (sysInfo.gppuInfo != null)
-                {
-                    sysInfo.gppuInfo.SystemInfoRef = sysId;
-                    db.Insert(sysInfo.gppuInfo);
-                }
-
-                // 6. درج Motherboard
-                if (sysInfo.motherboardInfo != null)
-                {
-                    sysInfo.motherboardInfo.SystemInfoRef = sysId;
-                    db.Insert(sysInfo.motherboardInfo);
-                }
-
-                // 7. درج RAM Summary
-                if (sysInfo.RamSummaryInfo != null)
-                {
-                    sysInfo.RamSummaryInfo.SystemInfoRef = sysId;
-                    db.Insert(sysInfo.RamSummaryInfo);
-                }
-
-                // 8. درج RAM Modules
-                if (sysInfo.RamModuleInfo != null && sysInfo.RamModuleInfo.Count > 0)
-                {
-                    foreach (var ram in sysInfo.RamModuleInfo)
-                    {
-                        ram.SystemInfoRef = sysId;
-                    }
-                    db.BulkInsert(sysInfo.RamModuleInfo);
-                }
-
-                // 9. درج Disk ها
-                if (sysInfo.DiskInfo != null && sysInfo.DiskInfo.Count > 0)
-                {
-                    foreach (var disk in sysInfo.DiskInfo)
-                    {
-                        disk.SystemInfoRef = sysId;
-                    }
-                    db.BulkInsert(sysInfo.DiskInfo);
-                }
-
-                // 10. درج Optical Drives
-                if (sysInfo.OpticalDriveInfo != null && sysInfo.OpticalDriveInfo.Count > 0)
-                {
-                    foreach (var od in sysInfo.OpticalDriveInfo)
-                        od.SystemInfoRef = sysId;
-                    db.BulkInsert(sysInfo.OpticalDriveInfo);
-                }
-
-                // 11. درج Monitors
-                if (sysInfo.monitorInfos != null && sysInfo.monitorInfos.Count > 0)
-                {
-                    foreach (var mon in sysInfo.monitorInfos)
-                        mon.SystemInfoRef = sysId;
-                    db.BulkInsert(sysInfo.monitorInfos);
-                }
-
-                // 12. درج Network Adapters
-                if (sysInfo.NetworkAdapterInfo != null && sysInfo.NetworkAdapterInfo.Count > 0)
-                {
-                    foreach (var nic in sysInfo.NetworkAdapterInfo)
-                        nic.SystemInfoRef = sysId;
-                    db.BulkInsert(sysInfo.NetworkAdapterInfo);
-                }
-
-                if (sysInfo.systemEnvironmentInfo != null )
-                {
-                    sysInfo.systemEnvironmentInfo.SystemInfoRef = sysId;
-                    db.Insert(sysInfo.systemEnvironmentInfo);
-                }
-
-                return sysId;
-            }
-            catch (Exception)
-            {
-                return -1;
-            }
         }
 
         public void AttachChildEntities()
